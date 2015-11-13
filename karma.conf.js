b67f9ad241d5c4effa259d157edc8bc65c23b073
Darren Lewis <darren@octono.com>
2015-11-13 20:05:49 +0000

    Added underscore dep for tests and refactor for show/hide buttons

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-messages/angular-messages.js',
+      'bower_components/underscore/underscore.js',
       '*.js',
       'test/**/*.js',
       'app/**/*.js'
