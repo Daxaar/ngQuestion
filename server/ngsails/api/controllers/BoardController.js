7e027aeef2e70fef79fc12fba8424bd917a01bcc
Darren Lewis <darren@octono.com>
2015-12-24 20:06:00 +0000

    added answers route to return all answers on a board

--- server/ngsails/api/controllers/BoardController.js
+++ server/ngsails/api/controllers/BoardController.js
@@ -7,4 +7,23 @@
 
 module.exports = {
 
+    answers : function (req,res) {
+      'use strict';
+      var answers = [];
+
+      Board.find()
+           .populate('questions')
+           .then(getAllAnswersOnBoard);
+
+      function getAllAnswersOnBoard ( boards ) {
+        boards.map(board => {
+          if(board.questions){
+            board.questions.map(question => {
+              answers.push({qid: question.id, answers: question.answers.map(a => a.text)});
+            });
+          }
+          res.json(answers);
+        });
+      }
+    }
 };
